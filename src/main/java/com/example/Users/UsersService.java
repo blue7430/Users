ef09bd5223cf89505871365a6af07bb30940b798
qnak9740 <qnak9740@naver.com>
2024-09-01 16:14:20 +0900

    데이터저장 함수 만번 반복되도록 변경

--- src/main/java/com/example/Users/UsersService.java
+++ src/main/java/com/example/Users/UsersService.java
@@ -10,10 +10,12 @@ public class UsersService {
     private final UsersRepository usersRepository;
 
     public void createUser(String username, String password, String email) {
+        for (int i = 0; i < 10000; i++) {
             Users users = new Users();
             users.setUsername(username);
             users.setPassword(password);
             users.setEmail(email);
             usersRepository.save(users);
+        }
     }
 }
