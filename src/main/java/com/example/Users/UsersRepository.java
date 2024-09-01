11cc71d583365073590b1b47ffcbcfd1ab9088c8
qnak9740 <qnak9740@naver.com>
2024-09-01 16:13:15 +0900

    service 파일에 데이터저장 함수 작성 (Repository 파일에 엔티티 파일 연결)

--- src/main/java/com/example/Users/UsersRepository.java
+++ src/main/java/com/example/Users/UsersRepository.java
@@ -1,4 +1,6 @@
 package com.example.Users;
 
-public interface UsersRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository <Users, Long>{
 }
--- src/main/java/com/example/Users/UsersService.java
+++ src/main/java/com/example/Users/UsersService.java
@@ -1,4 +1,19 @@
 package com.example.Users;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    public void createUser(String username, String password, String email) {
+            Users users = new Users();
+            users.setUsername(username);
+            users.setPassword(password);
+            users.setEmail(email);
+            usersRepository.save(users);
+    }
 }
