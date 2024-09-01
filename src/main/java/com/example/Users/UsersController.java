fb1fab01c5030a45a0557d4be40ddff8928f35db
qnak9740 <qnak9740@naver.com>
2024-09-01 16:17:05 +0900

    controller 파일에서 post 요청 보내기

--- src/main/java/com/example/Users/UsersController.java
+++ src/main/java/com/example/Users/UsersController.java
@@ -1,4 +1,23 @@
 package com.example.Users;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class UsersController {
+    private final UsersService usersService;
+
+    @GetMapping("/write")
+    String write(){
+        return "write";
+    }
+
+    @PostMapping("/create")
+    String createuser( String username, String password, String email) {
+        usersService.createUser(username, password, email);
+        return "redirect:/";
+    }
 }
