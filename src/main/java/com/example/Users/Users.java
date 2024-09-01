f2f3938e26416086107f6d5d27a995f5dadc31e6
qnak9740 <qnak9740@naver.com>
2024-09-01 16:08:19 +0900

    Users 테이블 생성 엔티티로 연결

--- src/main/java/com/example/Users/Users.java
+++ src/main/java/com/example/Users/Users.java
@@ -1,4 +1,22 @@
 package com.example.Users;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Data
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 100)
+    private String email;
 }
